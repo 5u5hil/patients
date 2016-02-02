f93cefae5f025b3939077f338f607cf419f91b96
sushil.sudhakaran <sushil@infiniteit.biz>
2016-02-02 11:48:34 +0530

    dd

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,12 +1,9 @@
 
 
 $(document).ready(function () {
-
-
     var elem = angular.element(document.querySelector('[ng-app]'));
     var injector = elem.injector();
     var $rootScope = injector.get('$rootScope');
-
     if (get('id') != null) {
         $rootScope.$apply(function () {
             $rootScope.userLogged = 1;
