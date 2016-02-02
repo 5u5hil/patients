fcf7fea0fc957fdac13537d48fa4b2e55eeb8c3e
sushil.sudhakaran <sushil@infiniteit.biz>
2016-02-02 16:03:47 +0530

    wwww

--- js/controllers.js
+++ js/controllers.js
@@ -1084,7 +1084,7 @@ angular.module('your_app_name.controllers', [])
         })
 
         .controller('PatientJoinCtrl', function ($ionicHistory, $window, $scope, $http, $stateParams, $sce, $filter, $timeout, $state, $ionicHistory) {
-            //$ionicHistory.clearCache();
+            $ionicHistory.clearCache();
             $scope.appId = $stateParams.id;
             $scope.mode = $stateParams.mode;
             $scope.userId = get('id');
