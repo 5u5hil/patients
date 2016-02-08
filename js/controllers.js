ad285b38214e7b922f48f1ed465db2b2705b973c
sushilcs111 <sushil.sud@gmail.com>
2016-02-08 18:12:56 +0530

    debug

--- js/controllers.js
+++ js/controllers.js
@@ -952,8 +952,8 @@ angular.module('your_app_name.controllers', [])
                         $ionicHistory.nextViewOptions({
                             disableBack: true
                         });
-					$state.go('app.thankyou', {'data': response.data}, {reload: true});
-					} else {
+                        $state.go('app.thankyou', {'data': response.data}, {reload: true});
+                    } else {
                         $state.go('app.Gopay', {'link': response.data});
                     }
                 }, function errorCallback(response) {
@@ -1160,7 +1160,8 @@ angular.module('your_app_name.controllers', [])
 
                 session.on({
                     streamDestroyed: function (event) {
-                         jQuery("#subscribersDiv").html("Doctor Left the Consultation");
+                        event.preventDefault();
+                        jQuery("#subscribersDiv").html("Doctor Left the Consultation");
                     },
                     streamCreated: function (event) {
                         subscriber = session.subscribe(event.stream, 'subscribersDiv', {width: "100%", height: "100%", subscribeToAudio: true});
@@ -1191,7 +1192,7 @@ angular.module('your_app_name.controllers', [])
                         alert("Error connecting: ", error.code, error.message);
 
                     } else {
-                        publisher = TB.initPublisher(event.stream, 'myPublisherDiv', {width: "200", height: "200",publishAudio: true});
+                        publisher = TB.initPublisher(event.stream, 'myPublisherDiv', {width: "200", height: "200", publishAudio: true});
                         session.publish(publisher);
 
                         var mic = 1;
