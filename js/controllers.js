58a1eeef0f09f2ca0731ef5d1943f39ca22849a8
sushilcs111 <sushil.sud@gmail.com>
2016-02-22 13:16:04 +0530

    date testt

--- js/controllers.js
+++ js/controllers.js
@@ -373,7 +373,7 @@ angular.module('your_app_name.controllers', [])
 
         .controller('AddRecordCtrl', function ($scope, $http, $state, $stateParams, $compile, $filter, $timeout, $ionicLoading) {
 			$scope.currDate = new Date();
-            $scope.curTime = $filter('date')(new Date(), 'yyyy-MM-dd');
+            $scope.curTime = $filter('date')(new Date(), 'dd/MM/yyyy');
             $scope.curT = $filter('date')(new Date(), 'HH:mm');
             $scope.userId = get('id');
             $scope.categoryId = $stateParams.id;
